0cfb3cbccebe8a4d62c374da8184f53c7c6c24fd
Washing-Mr8 <washing.22ml@gmail.com>
2025-10-01 13:23:00 +0200

    Duplicar la entrada

---
+++ fal_0.1/0_1.cpp
@@ -0,0 +1,44 @@
+﻿// Washington Morocho Lema
+// FAL-E51
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+
+using namespace std;
+
+// Resuelve un caso de prueba, leyendo de la entrada la configuración, y escribiendo la respuesta
+bool resuelveCaso() {
+    // leer los datos de la entrada
+    int numero = 0;
+
+    cin >> numero;
+
+    if (numero == 0)
+        return false;
+
+    // escribir sol
+    cout << numero*2 << "\n"; //duplicamos el numero si no es 0
+
+    return true;
+
+}
+
+int main() {
+    // Para la entrada por fichero.
+    // Comentar para acepta el reto
+#ifndef DOMJUDGE
+    std::ifstream in("0_1.in");
+    auto cinbuf = std::cin.rdbuf(in.rdbuf()); //save old buf and redirect std::cin to casos.txt
+#endif 
+
+    while (resuelveCaso());
+
+    // Para restablecer entrada. Comentar para acepta el reto
+#ifndef DOMJUDGE // para dejar todo como estaba al principio
+    std::cin.rdbuf(cinbuf);
+    system("PAUSE");
+#endif
+
+    return 0;
+}
